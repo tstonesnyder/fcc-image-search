09ecbf8743087ad46b4666925030446761a6e398
tstonesnyder <tstonesnyder@comcast.net>
2016-05-23 16:18:50 +0000

    Add models dir to repo -- oops

---
+++ app/models/searches.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// Create a new Mongoose schema.
+// Each Mongoose schema corresponds to a MongoDB collection. 
+// Each key in the schema defines and casts its corresponding property in the MongoDB document.
+var Search = new Schema(
+  {
+    search_string: { type: String, required: true},
+    user_ip: { type: String, required: true },
+    date_created: { type: Date, default: Date.now }
+  }
+);
+
+// Convert our schema to a Mongoose model, and export the resulting model.
+// The model is an object constructor that represents documents within the database.
+// args: (singular name of the collection in the db, schema name)
+module.exports = mongoose.model('Search', Search);
