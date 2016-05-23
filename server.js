555831d3abefaaef454ed0f0ef3ca5969c468944
tstonesnyder <tstonesnyder@comcast.net>
2016-05-23 07:14:24 +0000

    Fix default start nbr, update instructions page accordingly.

--- server.js
+++ server.js
@@ -10,17 +10,21 @@ var routes = require('./app/routes/index.js');
 var app = express();
 
 console.log(`app.settings.env: ${app.settings.env}`);
-console.log(`process.env.NODE_ENV: ${process.env.NODE_ENV}`);
 if (app.settings.env === 'development') {
   // ONLY NEED THIS IN DEV (on Heroku will store these)
   require('dotenv').config({path: '/home/ubuntu/private/.env-image-search'});
 }
 
 if (!process.env.GOOGLE_SEARCH_API_KEY || !process.env.GOOGLE_SEARCH_ENGINE_ID) {
-  console.log('ERROR: Missing environment variables!');
+  console.error('ERROR: Missing environment variables!');
   process.exit(1);
 }
 
+// This app will be running behind a proxy (at Cloud9 or at Heroku), 
+// so set this to get the correct ip address,
+// then it will use req.headers['x-forwarded-for']
+// instead of req.connection.remoteAddress.
+app.enable('trust proxy');
 
 // Use the port that Heroku provides or default to 8080 (for Cloud9):
 var port = process.env.PORT || 8080;
